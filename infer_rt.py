a2293858a826a9eb6bab08f953f479eefaaad24f
nickbild <Nicholas.Bild@sprint.com>
2019-07-02 16:27:24 -0400

    Start work on realtime inference.

---
+++ infer_rt.py
@@ -0,0 +1,77 @@
+import torch
+from torchvision.transforms import transforms
+import numpy as np
+from torch.autograd import Variable
+from torchvision.models import squeezenet1_1
+from io import open
+import os
+from PIL import Image
+from train import GestureNet
+import cv2
+from time import sleep
+import cv2
+
+
+# Load the saved model.
+checkpoint = torch.load("gestures_4.model")
+model = GestureNet(num_classes=2)
+model.load_state_dict(checkpoint)
+model.eval()
+
+
+def predict_image_class(image_path):
+    image = Image.open(image_path)
+    image = image.convert('RGB') # Model expects RGB.
+
+    transformation = transforms.Compose([
+        transforms.CenterCrop(600), # Image size defined here.
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Preprocess the image.
+    image_tensor = transformation(image).float()
+
+    # Add an extra batch dimension since pytorch treats all images as batches.
+    image_tensor = image_tensor.unsqueeze_(0)
+
+    if torch.cuda.is_available():
+        image_tensor.cuda()
+
+    # Turn the input into a Variable
+    input = Variable(image_tensor)
+
+    # Predict the class of the image.
+    output = model(input)
+    index = output.data.numpy().argmax()
+
+    # Look at output and get probabilities.
+
+    return index
+
+
+def gstreamer_pipeline (capture_width=3280, capture_height=2464, display_width=600, display_height=600, framerate=21, flip_method=0) :
+    return ('nvarguscamerasrc ! '
+    'video/x-raw(memory:NVMM), '
+    'width=(int)%d, height=(int)%d, '
+    'format=(string)NV12, framerate=(fraction)%d/1 ! '
+    'nvvidconv flip-method=%d ! '
+    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+    'videoconvert ! '
+    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+    if cap.isOpened():
+        while True:
+            ret_val, img = cap.read()
+            cv2.imwrite("current.jpg", img)
+            # Do this without writing image to disk.
+            index = predict_image_class("current.jpg")
+            print("Predicted Class: ", index)
+
+        cap.release()
+    else:
+        print 'Unable to open camera.'
---
+++ util/capture_gesture.py
@@ -0,0 +1,31 @@
+import cv2
+from time import sleep
+
+
+def gstreamer_pipeline (capture_width=3280, capture_height=2464, display_width=600, display_height=600, framerate=21, flip_method=0) :
+    return ('nvarguscamerasrc ! '
+    'video/x-raw(memory:NVMM), '
+    'width=(int)%d, height=(int)%d, '
+    'format=(string)NV12, framerate=(fraction)%d/1 ! '
+    'nvvidconv flip-method=%d ! '
+    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+    'videoconvert ! '
+    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+
+
+# Capture [num_images] images, spaced [delay_sec] seconds apart.
+def save_frame_sequence(num_images, delay_sec):
+    if cap.isOpened():
+        for i in range(num_images):
+            ret_val, img = cap.read()
+            cv2.imwrite("img/test_" + i + ".jpg", img)
+            sleep(delay_sec)
+
+        cap.release()
+    else:
+        print 'Unable to open camera.'
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    save_frame_sequence(10, 0.05)
