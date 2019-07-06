8cedcc02dd6af5c11a2af1acf2e8d2f419638b25
nickbild <Nicholas.Bild@sprint.com>
2019-07-06 19:16:27 -0400

    Add new trained model and data.

--- infer_rt.py
+++ infer_rt.py
@@ -17,7 +17,7 @@ img_height = 300
 
 
 # Load the saved model.
-checkpoint = torch.load("gestures_6.model")
+checkpoint = torch.load("gestures_11.model")
 model = GestureNet(num_classes=11)
 model.load_state_dict(checkpoint)
 model.eval()
